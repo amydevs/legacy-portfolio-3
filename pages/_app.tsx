1bd4770a9100c3beb0c5d28f5ec7788143647107
amy <ctrlalternativedelete@gmail.com>
2022-07-15 07:41:25 +1000

    made everything work through vars in css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,35 @@
-import '../styles/globals.scss'
+import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 import Link from 'next/link';
 import { useRouter } from 'next/router'
 
-import { routes, Route } from '../modules/routes'
+import { routes } from 'modules/routes'
+
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <div id='app'>
-      <nav>
-        { routes.map((e, i) => {
-          return (
-            <span key={i}>
-              <Link href={e.path} >
-                <a className={router.route == e.path ? 'active' : ''}>
-                  {e.name}
-                </a>
-              </Link>
-              {i === routes.length-1 ? '' : ' | '}
-            </span>
-          )
-        }) }
-      </nav>
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider>
+      <div id='app'>
+        <nav>
+          { routes.map((e, i) => {
+            return (
+              <span key={i}>
+                <Link href={e.path} >
+                  <a className={router.route == e.path ? 'active' : ''}>
+                    {e.name}
+                  </a>
+                </Link>
+                {i === routes.length-1 ? '' : ' | '}
+              </span>
+            )
+          }) }
+        </nav>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   )
 }
 
