7d58044daca4218aaf3679ab29f30908ba1a939e
amy <ctrlalternativedelete@gmail.com>
2022-07-15 04:07:45 +1000

    edited index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-
-import { mdiAbacus } from '@mdi/js';
+import styles from 'styles/Home.module.scss'
 
 import IconButton from 'components/IconButton'
 import SocialLinks from 'cfg/social'
