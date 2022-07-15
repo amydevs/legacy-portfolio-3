ba32309c2f3c3ece8bba464f43c48674ee855c1f
Amy Y <ctrlalternativedelete@gmail.com>
2022-07-15 17:51:13 +1000

    added custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+// 404.js
+import Donut from "components/Donut"
+
+const FourOhFour = () => {
+  return (
+    <div style={{textAlign: "center"}} className='fullscreen'>
+      <Donut />
+      <span className="title">404 - Page Not Found</span>
+    </div>
+  )
+}
+
+export default FourOhFour;
