e0bbf3c08c244ee1665715020a93bc0bdb37835f
Amy Y <ctrlalternativedelete@gmail.com>
2022-07-15 17:45:21 +1000

    made fetch dynamic

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ import rehypeRaw from 'rehype-raw'
 const About: NextPage = () => {
     const [mkdwn, set_mkdwn] = useState('');
 
-    fetch('https://raw.githubusercontent.com/jy1263/jy1263/master/README.md').then(async e => {
+    fetch(`https://raw.githubusercontent.com/${process.env.GH_USERNAME}/${process.env.GH_USERNAME}master/README.md`).then(async e => {
         set_mkdwn(await e.text());
     })
 
