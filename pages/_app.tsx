578407133898af465c3e5c0c5e46b4ec233aa659
Amy Y <ctrlalternativedelete@gmail.com>
2022-07-15 17:03:50 +1000

    added more open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,11 +18,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   const title = "Amy" + (found_route ? " - " + found_route.name : "");
   const image_src = `https://github.com/${process.env.GH_USERNAME}.png`;
+  const description = "Software Dev; Music Producer;";
 
   return (
     <ThemeProvider>
       <Head>
         <title>{title}</title>
+        <meta property="og:title" content={title} />
+
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+
         <link rel="image_src" href={image_src} />
         <meta property="og:image" content={image_src} />
       </Head>
