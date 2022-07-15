333525e46d2e53fa7ea1bc0e8a5c3db7894cf196
Amy Y <ctrlalternativedelete@gmail.com>
2022-07-15 17:52:52 +1000

    changed up 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,16 @@
 // 404.js
 import Donut from "components/Donut"
+import Head from "next/head";
 
 const FourOhFour = () => {
+  const fourohfour_text = "404 - This Page Could Not Be Found";
   return (
     <div style={{textAlign: "center"}} className='fullscreen'>
+      <Head>
+        <title>{fourohfour_text}</title>
+      </Head>
       <Donut />
-      <span className="title">404 - Page Not Found</span>
+      <span className="title">{fourohfour_text}</span>
     </div>
   )
 }
